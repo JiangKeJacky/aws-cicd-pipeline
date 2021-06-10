9f5e184531300c77e6ee05694beaa64a276fe4e8
dz902 <dz902i@gmail.com>
2021-06-10 14:49:15 +0800

    specify custom location for aws credentials

--- install-centos/code-deploy-register-instance.sh
+++ install-centos/code-deploy-register-instance.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+AWS_SHARED_CREDENTIALS_FILE='/etc/codedeploy-agent/conf/credentials'
 IAM_ROLE_ARN='iam::xxx'
 
 aws deploy register-on-premises-instance --instance-name INSTANCE_UNIQUE_NAME --iam-session-arn $IAM_ROLE_ARN
