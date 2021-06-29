56f6a40f8df62c6c2faac0deba0c173768ed5582
kejian <kejian@amazon.com>
2021-06-29 16:07:35 +0800

    integrate with sonar in pipeline

---
+++ spring-tests/scripts/status_app.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+APP_NAME=spring-test-unit*.jar
+#检查程序是否在运行
+is_exist(){
+  pid=`ps -ef|grep $APP_NAME|grep -v grep|awk '{print $2}' `
+  #如果不存在返回1，存在返回0
+  if [ -z "${pid}" ]; then
+   return 1
+  else
+    return 0
+  fi
+}
+  is_exist
+  if [ $? -eq "0" ]; then
+    echo "${APP_NAME} is running. Pid is ${pid}"
+  else
+    echo "${APP_NAME} is NOT running."
+  fi
+
+curl -I -s http://localhost  |head -1|awk '{ health = $2=="200"?"server is ok":"server is bad"}END{print health}'
